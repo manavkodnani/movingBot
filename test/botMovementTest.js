eaa5639fa0f7a54b5e8df255b25bcaa87c0be3fb
manavkodnani <manavkodnani@gmail.com>
2017-02-01 12:47:32 +0530

    Added negativetest case

--- test/botMovementTest.js
+++ test/botMovementTest.js
@@ -8,7 +8,7 @@ var gridY;
 var xposition;
 var yposition;
 var direction;
-var pathOfBot; 
+var pathOfBot;
 describe('When given valid input from inputFile.txt', function () {
   it('should return postion of x and y on the grid and the direction bot is facing', function () {
     gridX = 10;
@@ -19,4 +19,16 @@ describe('When given valid input from inputFile.txt', function () {
     pathOfBot = ['MMLMLMLML'];
     assert.equal(botMovement(gridX, gridY, xposition, yposition, direction, pathOfBot), 'Final position of bot is: 3 2 N');
   });
+
+  describe('When given invalid input from inputFile.txt', function () {
+    it('should return error when inputs are given in floating point', function () {
+      gridX = 10.5;
+      gridY = 20;
+      xposition = 3;
+      yposition = 3;
+      direction = 'N';
+      pathOfBot = ['MMLMLMLML'];
+      assert.equal(botMovement(gridX, gridY, xposition, yposition, direction, pathOfBot), 'Error: xGrids is not integer value');
+    })
+  })
 });
